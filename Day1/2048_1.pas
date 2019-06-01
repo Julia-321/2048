74a87d857eda20fbf4fafe4028e5427ab286eea5
Julia <hedviga258@gmail.com>
2019-06-01 16:28:28 +0300

    Initial commit

---
+++ Day1/2048_1.pas
@@ -0,0 +1,15 @@
+﻿uses GraphABC;
+
+var i, a: integer;
+BEGIN
+  a := 50;
+  setWindowSize(4*a, 4*a);
+  clearWindow(clLightGray);
+  setPenColor(clWhite);
+  setPenWidth(4);
+  for i := 0 to 4 do
+  begin
+    line(0, i*a, 4*a, i*a);
+    line(i*a, 0, i*a, 4*a);
+  end;
+END.
